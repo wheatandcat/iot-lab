52f7eb9e3770d553d6b136e3c2dc7d74fd120985
wheatandcat <wheatandcat@gmail.com>
2026-06-08 21:25:54 +0900

    fix

--- mailbox-sensor/mailbox-sensor.ino
+++ mailbox-sensor/mailbox-sensor.ino
@@ -1,24 +1,84 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "secrets.h"
+
 /**
- * @brief リードスイッチ確認
+ * @brief リードスイッチの状態変化をDiscordへ通知する
+ *
+ * @example
+ * 磁石を離して OPEN になったタイミングでDiscordに通知する。
  */
 const int reedPin = D2;
 
+int previousState = HIGH;
+
+/**
+ * @brief Wi-Fiへ接続する
+ */
+void connectWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+}
+
+/**
+ * @brief Discord Webhookへメッセージを送信する
+ *
+ * @param message Discordに送る本文
+ */
+void sendDiscordMessage(const String& message) {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+
+  HTTPClient http;
+  http.begin(DISCORD_WEBHOOK_URL);
+  http.addHeader("Content-Type", "application/json");
+
+  String payload = "{\"content\":\"" + message + "\"}";
+
+  int httpCode = http.POST(payload);
+
+  Serial.print("Discord response: ");
+  Serial.println(httpCode);
+
+  http.end();
+}
+
 void setup() {
   Serial.begin(115200);
+  delay(1000);
 
   pinMode(reedPin, INPUT_PULLUP);
 
-  Serial.println("Reed Test Start");
+  connectWiFi();
+
+  previousState = digitalRead(reedPin);
+
+  Serial.println("Mailbox sensor started");
 }
 
 void loop() {
-  int state = digitalRead(reedPin);
+  int currentState = digitalRead(reedPin);
 
-  if (state == LOW) {
-    Serial.println("CLOSED");
-  } else {
-    Serial.println("OPEN");
+  if (currentState != previousState) {
+    if (currentState == LOW) {
+      Serial.println("CLOSED");
+    } else {
+      Serial.println("OPEN");
+      sendDiscordMessage("📬 郵便受けが開きました");
+    }
+
+    previousState = currentState;
   }
 
-  delay(300);
+  delay(50);
 }
